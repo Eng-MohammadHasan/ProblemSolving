3ea77bb78b534bfef60a4eef4a44f72237b6b6f7
Mohammad_Hasan <mohammad.hasan.prof@gmail.com>
2024-10-03 23:33:07 +0300

    Add files via upload

---
+++ DiamondPattern.java
@@ -0,0 +1,29 @@
+package Diamond;
+public class DiamondPattern {
+
+    public static void main(String[] args) {
+        int size = 7; // Change the size of the diamond as needed
+
+        // Upper half of the diamond
+        for (int i = 1; i <= size; i++) {
+            for (int j = size; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        // Lower half of the diamond
+        for (int i = size - 1; i >= 1; i--) {
+            for (int j = size; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
